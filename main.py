eacb4e35ecf3e8ae16b2213e87e674defb71bfe9
michael-maple-waffles <michael.willes@ucas-edu.net>
2026-01-20 17:42:29 +0000

    made chars list

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Holds user interface, runs program
 
+chars = []
+
 def main():
     print("You can either: \n\t1. Create a character  \n\t2. Exit the program.")
     choice = input("What do you want to do?(1/2): ")
