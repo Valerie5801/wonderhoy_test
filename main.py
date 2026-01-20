50c89972d6c447e6202ff8c1a23cf4ff17a12b8e
Valerie5801 <valerie.yang@ucas-edu.net>
2026-01-20 10:33:31 -0700

    Added the option for the user to choose 1 (create a character) 2 (exit the program)

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #Holds user interface, runs program
 
 def main():
-    pass
+    print("You can either: \n\t1. Create a character  \n\t2. Exit the program.")
+    choice = input("What do you want to do?(1/2): ")
+    if choice == "1":
+        pass
+    elif choice == "2":
+        pass
+
 main()
