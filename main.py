8633dd37fc18c35f1200ba2468c2c112c97633bb
michael-maple-waffles <michael.willes@ucas-edu.net>
2026-01-20 17:45:25 +0000

    made chars appendable by choice

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     print("You can either: \n\t1. Create a character  \n\t2. Exit the program.")
     choice = input("What do you want to do?(1/2): ")
     if choice == "1":
-        pass
+        chars.append(choice)
     elif choice == "2":
         pass
 
